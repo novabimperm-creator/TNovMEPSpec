5afabef8f6ec02357f79ab416a608d6cb13c4c59
novabimperm-creator <roshior.a@pm-nova.ru>
2026-09-10 08:53:57 +0500

    добавлены предпроверки сс

--- Properties/VersionInfo.cs
+++ Properties/VersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.26.0908.1034")]
-[assembly: AssemblyFileVersion("2.26.0908.1034")]
+[assembly: AssemblyVersion("2.26.0909.999")]
+[assembly: AssemblyFileVersion("2.26.0909.999")]
